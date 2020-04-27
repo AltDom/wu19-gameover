04132d463ebe63af64edf5aa48f5c941ee1047f8
Jesper <jesper.lundqvist@gmail.com>
2020-04-27 20:57:02 +0200

    added logic for making the trail

--- classes/curve.js
+++ classes/curve.js
@@ -4,6 +4,8 @@ class Curve {
     this.y = 0;
     this.increment = 3;
     this.angle = Math.PI / 2;
+    this.total = 0;
+    this.tail = [];
   }
 
   updateAngle(value) {
@@ -11,11 +13,18 @@ class Curve {
   }
 
   update() {
+    this.total++;
+
+    this.tail[this.total - 1] = createVector(this.x, this.y);
+
     this.x = this.x + this.increment * Math.sin(this.angle);
     this.y = this.y + this.increment * Math.cos(this.angle);
   }
 
   show() {
+    for (let i = 0; i < this.total; i++) {
+      circle(this.tail[i].x, this.tail[i].y, 5);
+    }
     fill(255);
     circle(this.x, this.y, 5);
   }
