f3a53eab17fea9d1f9986e1e58f9ca52b3c272a9
Jesper <jesper.lundqvist@gmail.com>
2020-05-04 17:34:41 +0200

    added function to track collisions

--- classes/curve.js
+++ classes/curve.js
@@ -4,6 +4,9 @@ class Curve {
     this.y = 0;
     this.increment = 3;
     this.angle = Math.PI / 2;
+    this.colors = {
+      player1: [255, 204, 0],
+    };
   }
 
   updateAngle(value) {
@@ -12,7 +15,22 @@ class Curve {
 
   update() {
     circle(this.x, this.y, 5);
+    let c = color(this.colors.player1);
+    fill(c);
+    noStroke();
     this.x = this.x + this.increment * Math.sin(this.angle);
     this.y = this.y + this.increment * Math.cos(this.angle);
-  };
+  }
+
+  collision() {
+    const currentPostion = get(this.x, this.y);
+
+    if (
+      currentPostion[0] === this.colors.player1[0] &&
+      currentPostion[1] === this.colors.player1[1] &&
+      currentPostion[2] === this.colors.player1[2]
+    ) {
+      console.log('GAME ENDED');
+    }
+  }
 }
